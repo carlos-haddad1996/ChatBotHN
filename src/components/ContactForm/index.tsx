c89dacc7d959839e76d0d8d56fc35ae155ffd0cb
Jose Medina <JoseMedina96@unitec.edu>
2021-06-15 13:24:48 -0600

    Cambios de contenido

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -38,7 +38,7 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
               <Col span={24}>
                 <Input
                   type="text"
-                  name="name"
+                  name="Nombre"
                   placeholder="Your Name"
                   value={values.name || ""}
                   onChange={handleChange}
@@ -48,7 +48,7 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
               <Col span={24}>
                 <Input
                   type="text"
-                  name="email"
+                  name="Correo electrónico"
                   placeholder="Your Email"
                   value={values.email || ""}
                   onChange={handleChange}
@@ -59,7 +59,7 @@ const Contact = ({ title, content, id, t }: ContactProps) => {
                 <TextArea
                   placeholder="Your Message"
                   value={values.message || ""}
-                  name="message"
+                  name="Comentario"
                   onChange={handleChange}
                 />
                 <ValidationType type="message" />
