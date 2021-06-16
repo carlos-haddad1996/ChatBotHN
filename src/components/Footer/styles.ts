bdbed78c06cc4a97e3da52c9f389839397a8f49b
Jose Medina <JoseMedina96@unitec.edu>
2021-06-16 15:24:33 -0600

    el cambio de verdad

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -88,7 +88,7 @@ export const Empty = styled("div")`
 `;
 
 export const FooterContainer = styled("div")`
-  max-width: 510px;
+  max-width: 95px;
   width: 100%;
   display: flex;
   justify-content: space-between;
